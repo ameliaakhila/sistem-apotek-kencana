c8d9dea8254e000c0451958cf6fd071054c3ea08
kuriyantoadi <melatikuning212@gmail.com>
2024-06-29 16:41:27 +0700

    update database

--- database/sistem-apotek-kencana.sql
+++ database/sistem-apotek-kencana.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Waktu pembuatan: 28 Jun 2024 pada 15.38
--- Versi server: 10.4.28-MariaDB
--- Versi PHP: 8.2.4
+-- Host: localhost
+-- Generation Time: Jun 29, 2024 at 11:40 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 5.6.40
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_detail_permintaan_obat`
+-- Table structure for table `tb_detail_permintaan_obat`
 --
 
 CREATE TABLE `tb_detail_permintaan_obat` (
@@ -32,12 +33,12 @@ CREATE TABLE `tb_detail_permintaan_obat` (
   `id_permintaan_obat` int(11) NOT NULL,
   `id_obat` int(11) NOT NULL,
   `jumlah_obat` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_faktur`
+-- Table structure for table `tb_faktur`
 --
 
 CREATE TABLE `tb_faktur` (
@@ -45,26 +46,55 @@ CREATE TABLE `tb_faktur` (
   `no_faktur` int(11) NOT NULL,
   `tgl_faktur` varchar(20) NOT NULL,
   `keterangan` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_obat`
+-- Table structure for table `tb_jenis_obat`
+--
+
+CREATE TABLE `tb_jenis_obat` (
+  `id_jenis_obat` int(11) NOT NULL,
+  `nama_jenis_obat` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_jenis_obat`
+--
+
+INSERT INTO `tb_jenis_obat` (`id_jenis_obat`, `nama_jenis_obat`) VALUES
+(1, 'parasetamol'),
+(2, 'parasetamol3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_obat`
 --
 
 CREATE TABLE `tb_obat` (
   `id_obat` int(11) NOT NULL,
   `nama_obat` varchar(100) NOT NULL,
-  `jenis_obat` varchar(50) NOT NULL,
-  `satuan` varchar(20) NOT NULL,
-  `stok_obat` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `satuan_obat` varchar(20) NOT NULL,
+  `stok_obat` int(11) NOT NULL,
+  `id_jenis_obat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_obat`
+--
+
+INSERT INTO `tb_obat` (`id_obat`, `nama_obat`, `satuan_obat`, `stok_obat`, `id_jenis_obat`) VALUES
+(3, 'bodrex0', 'Box', 0, 1),
+(4, 'bodrex1', 'Botol', 0, 0),
+(5, 'bodrex2', 'Botol ', 0, 2),
+(6, 'bodrex00', 'Botol', 0, 1);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_obat_masuk`
+-- Table structure for table `tb_obat_masuk`
 --
 
 CREATE TABLE `tb_obat_masuk` (
@@ -72,12 +102,12 @@ CREATE TABLE `tb_obat_masuk` (
   `id_faktur` int(11) NOT NULL,
   `jumlah_obat` int(11) NOT NULL,
   `tgl_kadaluarsa` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_permintaan_obat`
+-- Table structure for table `tb_permintaan_obat`
 --
 
 CREATE TABLE `tb_permintaan_obat` (
@@ -86,99 +116,86 @@ CREATE TABLE `tb_permintaan_obat` (
   `kd_permintaan_obat` int(11) NOT NULL,
   `tgl_permintaan_obat` varchar(20) NOT NULL,
   `keterangan` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_user`
+-- Table structure for table `tb_user`
 --
 
 CREATE TABLE `tb_user` (
   `id_user` int(11) NOT NULL,
-  `username` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `password` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `status` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `username` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `password` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `status` varchar(50) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_user`
+-- Dumping data for table `tb_user`
 --
 
 INSERT INTO `tb_user` (`id_user`, `username`, `password`, `status`) VALUES
-(2, 'gudang', 'a80dd043eb5b682b4148b9fc2b0feabb2c606fff', 'gudang'),
-(3, 'petugas', '670489f94b6997a870b148f74744ee5676304925', 'petugas'),
-(4, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin'),
-(5, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin'),
-(6, 'admin2', '315f166c5aca63a157f7d41007675cb44a948b33', '');
+(1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin'),
+(2, 'gudang', 'a80dd043eb5b682b4148b9fc2b0feabb2c606fff', 'petugas'),
+(3, 'petugas1', '8cb2237d0679ca88db6464eac60da96345513964', 'petugas'),
+(4, 'admin2', '315f166c5aca63a157f7d41007675cb44a948b33', 'admin');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `tb_detail_permintaan_obat`
+-- Indexes for table `tb_detail_permintaan_obat`
 --
 ALTER TABLE `tb_detail_permintaan_obat`
   ADD PRIMARY KEY (`id_detail_permintaan_obat`);
 
 --
--- Indeks untuk tabel `tb_faktur`
+-- Indexes for table `tb_jenis_obat`
 --
-ALTER TABLE `tb_faktur`
-  ADD PRIMARY KEY (`id_faktur`);
+ALTER TABLE `tb_jenis_obat`
+  ADD PRIMARY KEY (`id_jenis_obat`);
 
 --
--- Indeks untuk tabel `tb_obat_masuk`
+-- Indexes for table `tb_obat`
 --
-ALTER TABLE `tb_obat_masuk`
-  ADD PRIMARY KEY (`id_obat_masuk`);
+ALTER TABLE `tb_obat`
+  ADD PRIMARY KEY (`id_obat`);
 
 --
--- Indeks untuk tabel `tb_permintaan_obat`
---
-ALTER TABLE `tb_permintaan_obat`
-  ADD PRIMARY KEY (`id_permintaan_obat`);
-
---
--- Indeks untuk tabel `tb_user`
+-- Indexes for table `tb_user`
 --
 ALTER TABLE `tb_user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `tb_detail_permintaan_obat`
+-- AUTO_INCREMENT for table `tb_detail_permintaan_obat`
 --
 ALTER TABLE `tb_detail_permintaan_obat`
   MODIFY `id_detail_permintaan_obat` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_faktur`
+-- AUTO_INCREMENT for table `tb_jenis_obat`
 --
-ALTER TABLE `tb_faktur`
-  MODIFY `id_faktur` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tb_jenis_obat`
+  MODIFY `id_jenis_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_obat_masuk`
+-- AUTO_INCREMENT for table `tb_obat`
 --
-ALTER TABLE `tb_obat_masuk`
-  MODIFY `id_obat_masuk` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tb_obat`
+  MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_permintaan_obat`
---
-ALTER TABLE `tb_permintaan_obat`
-  MODIFY `id_permintaan_obat` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `tb_user`
+-- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
