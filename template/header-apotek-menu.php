4b4f8ba0228f1a1b4f7a36b476d2ae2c4e1ffdaa
kuriyantoadi <melatikuning212@gmail.com>
2024-08-10 23:17:01 +0700

    update menu kadaluarsa

--- template/header-apotek-menu.php
+++ template/header-apotek-menu.php
@@ -13,27 +13,15 @@
 							<span class="nav-text">Permintaan Obat</span>
 						</a>
 					</li>
-
-                    <!-- <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
-							<i class="fas fa-bars"></i>
-							<span class="nav-text">Sub Menu</span>
+					<li>
+						<a href="../apotek/obat_kadaluarsa.php">
+							<i class="fa fa-exclamation-circle"></i>
+							<span class="nav-text">Obat Kadaluarsa</span>
 						</a>
-                        <ul aria-expanded="false">
-							<li><a href="index.html">Dashboard Light</a></li>
-							<li><a href="index-2.html">Dashboard Dark</a></li>
-							<li><a href="kanban.html">Kanban</a></li>
-							<li><a href="clients.html">Clients</a></li>
-							<li><a href="project-details.html">Project Details</a></li>
-							<li><a href="message.html">Messages</a></li>
-							<li><a href="latest-activity.html">Latest Activity</a></li>	
-						</ul>
-
-                    </li> -->
-					                                    
+					</li>
+                                    
                     
                 </ul>
-				
-				
-				
+								
 			</div>
         </div>
